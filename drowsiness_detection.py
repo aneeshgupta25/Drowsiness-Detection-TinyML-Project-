42b4f3cc4b689df8f697565aa9aefedc28d5bc92
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-01-29 23:20:10 +0530

    Detect Face and Right Eye

--- drowsiness_detection.py
+++ drowsiness_detection.py
@@ -2,13 +2,54 @@ import cv2
 from pygame import mixer
 
 mixer.init()
-sound = mixer.Sound('alarm.wav')
+sound = mixer.Sound("alarm.wav")
 
-face = cv2.CascadeClassifier('haar cascade files\haarcascade_frontalface_alt.xml')
-leye = cv2.CascadeClassifier('haar cascade files\haarcascade_lefteye_2splits.xml')
-reye = cv2.CascadeClassifier('haar cascade files\haarcascade_righteye_2splits.xml')
+face = cv2.CascadeClassifier("haar cascade files\haarcascade_frontalface_alt.xml")
+leye = cv2.CascadeClassifier("haar cascade files\haarcascade_lefteye_2splits.xml")
+reye = cv2.CascadeClassifier("haar cascade files\haarcascade_righteye_2splits.xml")
 
-lbl=['Close','Open']
+lbl = ["Close", "Open"]
 
 cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
+path = os.getcwd()
+cap = cv2.VideoCapture(0)
+count = 0
+score = 0
+thicc = 2
+rpred = [99]
+lpred = [99]
+
+while True:
+    ret, frame = cap.read()
+    height, width = frame.shape[:2]
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face.detectMultiScale(
+        gray, minNeighbors=5, scaleFactor=1.1, minSize=(25, 25)
+    )
+    left_eye = leye.detectMultiScale(gray)
+    right_eye = reye.detectMultiScale(gray)
+
+    cv2.rectangle(
+        frame, (0, height - 50), (200, height), (0, 0, 0), thickness=cv2.FILLED
+    )
+
+    for x, y, w, h in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (100, 100, 100), 1)
+
+    for x, y, w, h in right_eye:
+        r_eye = frame[y : y + h, x : x + w]
+        count = count + 1
+        r_eye = cv2.cvtColor(r_eye, cv2.COLOR_BGR2GRAY)
+        r_eye = cv2.resize(r_eye, (24, 24))
+        r_eye = r_eye / 255
+        r_eye = r_eye.reshape(24, 24, -1)
+        r_eye = np.expand_dims(r_eye, axis=0)
+        rpred = np.argmax(model.predict(r_eye), axis=1)
+        if rpred[0] == 1:
+            lbl = "Open"
+        if rpred[0] == 0:
+            lbl = "Closed"
+        break
